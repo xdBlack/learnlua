45db26248d49baa419db7c133a3056df5cb93c8c
wangc <1214953363@qq.com>
2021-07-18 08:29:32 +0800

    append first, add table.lua

--- first.lua
+++ first.lua
@@ -1,4 +1,39 @@
+--------------------
 print("Hello, lua!")
+--------------------
 
+--------------------
 num = 100
 print(num)
+--------------------
+
+--------------------
+function sayHello()
+    print("Hello, lua!")
+end
+sayHello()
+--------------------
+
+--------------------
+function maxlua(a, b)
+    return a > b and a or b
+end
+print(maxlua(3, 5))
+--------------------
+
+--------------------
+function maxNormal(a, b)
+    if a > b then
+        return a
+    else
+        return b
+    end
+end
+print(maxNormal(3, 6))
+--------------------
+
+--------------------
+for var=1,100 do
+    print(var)
+end
+--------------------
---
+++ table.lua
@@ -0,0 +1,13 @@
+--------------------------------
+config = {}
+config.word = "hello"
+config.num = 100
+config["name"] = "zhangSan"
+print(config["word"])
+print(config.name)
+--------------------------------
+
+--------------------------------
+person = {name="wangchong", age=24, iswork=true}
+print(person.name, person.age, person.iswork)
+--------------------------------
